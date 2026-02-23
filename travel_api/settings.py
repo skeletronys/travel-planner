adaf41d97fa514331b0a8502b32fa462c67734ce
Yevhen <mykytcey@gmail.com>
2026-02-23 14:57:05 +0100

    Added admin.py and jazzmin admin panel

--- travel_api/settings.py
+++ travel_api/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'django_filters',
     'corsheaders',
-    'core',
+    'trips',
 ]
 
 MIDDLEWARE = [
--- trips/admin.py
+++ trips/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import TravelProject, ProjectPlace
 
-# Register your models here.
+
+@admin.register(TravelProject)
+class TravelProjectAdmin(admin.ModelAdmin):
+    list_display = ["name", "user", "start_date", "created_at"]
+    list_filter = ["start_date"]
+    search_fields = ["name", "user__username"]
+
+
+@admin.register(ProjectPlace)
+class ProjectPlaceAdmin(admin.ModelAdmin):
+    list_display = ["title", "project", "external_id", "visited", "created_at"]
+    list_filter = ["visited"]
+    search_fields = ["title", "artist"]
